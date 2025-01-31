b9584b15e888aac1dd87bb078179af743816300b
BHUVANESH09-HUB <bhuvanesh22td0660@svcet.ac.in>
2025-01-31 10:21:01 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,13 @@
+# calculator
+def add(a,b):#for add 
+    return a+b
+def subract(a,b):#for subract
+    return a-b
+def multiply(a,b):#for multiply
+    return a*b
+def division(a,b):#for division
+    return a/b
+def module(a,b):#for module
+    return a%b
+def square(a):#for square
+    return a**2 
